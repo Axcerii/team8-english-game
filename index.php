21ffb24643a84447f6f4700a4a55a9cc8307d048
Hugo Malezet <malezethp@gmail.com>
2024-12-03 12:11:08 +0100

    Correction des problèmes lié au JSON et ajout d'une petite partie graphique

--- index.php
+++ index.php
@@ -8,6 +8,37 @@
     <title>Brand and Butter</title>
 </head>
 <body>
+    <section class='beta'>
+        <table>
+            <tr>
+                <th>Revenue</th>
+                <th>Budget</th>
+                <th>Prospects</th>
+                <th>Turn</th>
+                <th>Next Turn</th>
+                <th>Use a Card</th>
+            </tr>
+            <tr>
+                <td id='revenue'></td>
+                <td id='budget'></td>
+                <td id='prospects'></td>
+                <td id='turn'></td>
+                <td>
+                    <button onclick='Player1.nextTurn()'>
+                        Next Turn
+                    </button>
+                </td>
+                <td>
+                    <input type="numecric" id='cardId' min='100' max='700'>
+                    <button onclick='globalUseCard()'>
+                        Use Card
+                    </button>
+                </td>
+            </tr>
+        </table>
+
+        
+    </section>
     <section class='bar'>
         
     </section>
--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-class Joueur {
+var cards = [];
+
+fetch('cards.json')
+  .then(response => response.json())
+  .then(data => {
+    cards = data;
+    console.log("Cards Loaded :", cards);
+  })
+  .catch(error => console.error("Error while loading cards :", error));
+
+class Player {
     constructor(name) {
       this.name = name;
       this.budget = 100000; 
       this.revenue = 0;
 //    this.clients = 0;
       this.prospects = 0;
+      this.turn = 1;
+
+      this.budgetDisplayer = document.getElementById("budget");
+      this.revenueDisplayer = document.getElementById("revenue");
+      this.prospectsDisplayer = document.getElementById("prospects");
+      this.turnDisplayer = document.getElementById("turn");
     }
   
     useCard(card) {
-      for (const [key, effet] of Object.entries(cards.effects)) {
+      for (const [key, effet] of Object.entries(card.effects)) {
         if (!effet || !effet.operation) continue;
         
         switch (effet.operation) {
@@ -29,4 +45,51 @@ class Joueur {
         }
       }
     }
+
+    nextTurn(){
+      this.turn++;
+      this.budget = this.revenue*0.1 + this.budget;
+
+      let prospectsValue = 100;
+
+      if(this.prospects > 1000){
+        this.revenue += 1000*prospectsValue;
+        this.prospects -= 1000;
+      }
+      else{
+        this.revenue += prospectsValue*this.prospects;
+        this.prospects = 0;
+      }
+
+      this.printInfosPlayer();
+    }
+
+    printInfosPlayer(){
+      this.budgetDisplayer.innerHTML = this.budget;
+      this.revenueDisplayer.innerHTML = this.revenue;
+      this.prospectsDisplayer.innerHTML = this.prospects;
+      this.turnDisplayer.innerHTML = this.turn;
+    }
+  } /* End of Player's Class */
+
+const Player1 = new Player("Player 1");
+
+Player1.printInfosPlayer();
+
+
+/* Functions */
+
+function globalUseCard() {
+
+  const cardId = parseInt(document.getElementById("cardId").value, 10);
+
+  const myCard = cards.find(card => card.id === cardId);
+
+  if (!myCard) {
+    console.error("card with id", cardId, "not found", cardId);
+    return;
   }
+
+  Player1.useCard(myCard);
+  Player1.printInfosPlayer();
+}
